fed2ed71a2a564c644ced6ffb123ad776f87c230
hannah lewandowski <hannah.lewandowski@gmail.com>
2017-10-11 19:49:15 -0500

    created bird model class; added refs in csproj

---
+++ Models/Bird.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Birdwatcher.Models{
+    public class Bird{
+        public int ID {get; set; }
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public string Region {get; set; }
+        public string ImageLocation {get; set; }
+
+
+    }
+}
