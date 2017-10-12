8d01228f89fc9eadf4a3e58806d7f5ed0c5b1c79
hannah lewandowski <hannah.lewandowski@gmail.com>
2017-10-11 20:21:12 -0500

    generated User CRUD scaffolding:wq

--- Models/BirdwatcherContext.cs
+++ Models/BirdwatcherContext.cs
@@ -4,5 +4,6 @@ namespace Birdwatcher.Models{
     public class BirdwatcherContext : DbContext{
         public BirdwatcherContext (DbContextOptions<BirdwatcherContext> options) : base(options){}
         public DbSet<Birdwatcher.Models.Bird> Bird {get; set;}
+        public DbSet<Birdwatcher.Models.User> User {get; set; }
     }
 }
