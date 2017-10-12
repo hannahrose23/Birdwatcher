fd9de675a1d495628c795273ded397852695570f
hannah lewandowski <hannah.lewandowski@gmail.com>
2017-10-11 19:56:33 -0500

    added BirdwatcherContext.cs; included db in startup services

---
+++ Models/BirdwatcherContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Birdwatcher.Models{
+    public class BirdwatcherContext : DbContext{
+        public BirdwatcherContext (DbContextOptions<BirdwatcherContext> options) : base(options){
+            public DbSet<Birdwatcher.Models.Bird> Bird {get; set;}
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Birdwatcher.Models;
 
 namespace Birdwatcher
 {
@@ -22,6 +24,8 @@ namespace Birdwatcher
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddDbContext<BirdwatcherContext>(options => options.UseSqlite("Data Source=Birdwatcher.db"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
