c10c80d0284b00eb010ae80452ae005f786cce2b
hannah lewandowski <hannah.lewandowski@gmail.com>
2017-10-11 20:08:50 -0500

    added CRUD operations for Birds

---
+++ Controllers/BirdsController.cs
@@ -0,0 +1,152 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Birdwatcher.Models;
+
+namespace Birdwatcher.Controllers
+{
+    public class BirdsController : Controller
+    {
+        private readonly BirdwatcherContext _context;
+
+        public BirdsController(BirdwatcherContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Birds
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Bird.ToListAsync());
+        }
+
+        // GET: Birds/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bird = await _context.Bird
+                .SingleOrDefaultAsync(m => m.ID == id);
+            if (bird == null)
+            {
+                return NotFound();
+            }
+
+            return View(bird);
+        }
+
+        // GET: Birds/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Birds/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Name,Color,Region,ImageLocation")] Bird bird)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(bird);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(bird);
+        }
+
+        // GET: Birds/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bird = await _context.Bird.SingleOrDefaultAsync(m => m.ID == id);
+            if (bird == null)
+            {
+                return NotFound();
+            }
+            return View(bird);
+        }
+
+        // POST: Birds/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Name,Color,Region,ImageLocation")] Bird bird)
+        {
+            if (id != bird.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(bird);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BirdExists(bird.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(bird);
+        }
+
+        // GET: Birds/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bird = await _context.Bird
+                .SingleOrDefaultAsync(m => m.ID == id);
+            if (bird == null)
+            {
+                return NotFound();
+            }
+
+            return View(bird);
+        }
+
+        // POST: Birds/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var bird = await _context.Bird.SingleOrDefaultAsync(m => m.ID == id);
+            _context.Bird.Remove(bird);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BirdExists(int id)
+        {
+            return _context.Bird.Any(e => e.ID == id);
+        }
+    }
+}
--- Models/BirdwatcherContext.cs
+++ Models/BirdwatcherContext.cs
@@ -2,8 +2,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Birdwatcher.Models{
     public class BirdwatcherContext : DbContext{
-        public BirdwatcherContext (DbContextOptions<BirdwatcherContext> options) : base(options){
-            public DbSet<Birdwatcher.Models.Bird> Bird {get; set;}
-        }
+        public BirdwatcherContext (DbContextOptions<BirdwatcherContext> options) : base(options){}
+        public DbSet<Birdwatcher.Models.Bird> Bird {get; set;}
     }
 }
